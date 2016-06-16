71a1bf597adca300e33803243b051ad95f4ccc88
Fabian Schuiki <fabian@schuiki.ch>
2016-06-16 15:02:35 +0200

    Setup project

---
+++ src/common.h
@@ -0,0 +1,10 @@
+/* Copyright (c) 2016 Fabian Schuiki */
+#pragma once
+#include <assert.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "config.h"
